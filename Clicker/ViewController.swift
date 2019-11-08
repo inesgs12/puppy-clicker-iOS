79f524f0e552b4821a43b08ab7e6fc070b4b31ef
Ines Guerrero <inesgs@gmail.com>
2019-11-08 18:41:55 +0000

    added gitignore and Readme

--- Clicker/ViewController.swift
+++ Clicker/ViewController.swift
@@ -9,12 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var counter = 0
+    let colors = [UIColor.purple, UIColor(named: "orangeish"), UIColor(named: "pinkish"), UIColor(named: "blueish")]
+    let numberOfClicks = 5
+  
+    
+    @IBOutlet weak var counterLabel: UILabel!
+     @IBOutlet weak var increaseButtonImage: UIButton!
+    @IBOutlet weak var decreaseButtonImage: UIButton!
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        print("Hello, world")
+        counter += 1
+        counterLabel.text = "\(counter)"
+          let index = abs(counter / numberOfClicks % colors.count)
+        view.backgroundColor = colors[index]
+        if counter > 10 {
+            increaseButtonImage.setImage(UIImage(named: "puppy-emoji-png-3"), for: .normal)
+             decreaseButtonImage.setImage(nil, for: .normal)
+            
+        }
     }
-
-
+    
+    @IBAction func decreaseCounter(_ sender: Any) {
+        counter -= 1
+        counterLabel.text = "\(counter)"
+          let index = abs(counter / numberOfClicks % colors.count)
+        view.backgroundColor = colors[index]
+        if counter < 10 {
+            decreaseButtonImage.setImage(UIImage(named: "labrador"), for: .normal)
+            increaseButtonImage.setImage(nil, for: .normal)
+           
+        }
+    }
+    
+   
 }
 
